85ccfdff5a5d3f0dfa1aad8d61d4186cff5d72b4
jmirav <jmira420@gmail.com>
2018-01-15 13:35:55 -0500

    Ajusta consulta para gastos del usuario logueado

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,7 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-     @expenses = Expense.order("date DESC")
+     @expenses = Expense.order("date DESC").where(user_id: current_user)
      if params[:concept].present?
        @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%").where(user_id: current_user)
      end
